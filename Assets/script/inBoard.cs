864050612eba9d7575df1ac434fd6f62856d7c68
Rex Lucas <137895856+BHASVIC-rexlucas22@users.noreply.github.com>
2023-11-24 15:40:41 +0000

    Update

--- Assets/script/inBoard.cs
+++ Assets/script/inBoard.cs
@@ -10,14 +10,15 @@ public class inBoard : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if ((collision.gameObject.CompareTag("Red")) && (Piece.isLaunched == true))
+        bool isLaunched = collision.GetComponent<Collider2D>();
+        if ((collision.gameObject.CompareTag("Red")) && (isLaunched = true))
         {
             pieceBoard = collision.gameObject;
             //makes it so piece only deletes if it's been launched
             Destroy(pieceBoard, 5);
             //waits 5 seconds before deleting object
         }
-        if ((collision.gameObject.CompareTag("Yellow")) && (Piece.isLaunched == true))
+        if ((collision.gameObject.CompareTag("Yellow")) && (isLaunched = true))
         {
             pieceBoard = collision.gameObject;
             Destroy(pieceBoard, 5);
